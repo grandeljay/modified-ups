934104ee2eb5ce948b89132c88a973c3f3a9dbce
grandeljay <github@grandels.email>
2024-02-26 08:47:47 +0100

    chore(module): update to v0.3.11

--- src/includes/modules/shipping/grandeljayups.php
+++ src/includes/modules/shipping/grandeljayups.php
@@ -21,7 +21,7 @@ use RobinTheHood\ModifiedStdModule\Classes\{StdModule, CaseConverter};
 class grandeljayups extends StdModule
 {
     public const NAME    = 'MODULE_SHIPPING_GRANDELJAYUPS';
-    public const VERSION = '0.3.10';
+    public const VERSION = '0.3.11';
 
     public static array $methods_international = [
         Group::SHIPPING_GROUP_A,
