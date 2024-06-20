9e21b8e9e99ebcb1d7c6ed476351e3b095f60b0a
grandeljay <github@grandels.email>
2024-06-20 09:34:40 +0200

    chore(module): update to v0.10.0

--- src/includes/modules/shipping/grandeljayups.php
+++ src/includes/modules/shipping/grandeljayups.php
@@ -20,7 +20,7 @@ use RobinTheHood\ModifiedStdModule\Classes\{StdModule, CaseConverter};
  */
 class grandeljayups extends StdModule
 {
-    public const VERSION = '0.9.0';
+    public const VERSION = '0.10.0';
 
     private Module $module;
 
