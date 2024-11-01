94b027c8a8197be68445c56c3f642cbde109a549
grandeljay <github@grandels.email>
2024-11-01 10:33:55 +0100

    refactor: add global namespace to global functions

--- src-mmlc/Configuration/Configuration.php
+++ src-mmlc/Configuration/Configuration.php
@@ -12,14 +12,14 @@ class Configuration
         $constant_value = $default;
 
         try {
-            $constant_value = constant($constant_key);
+            $constant_value = \constant($constant_key);
         } catch (\Throwable $throwable) {
             global $LogLevel;
 
             $logger = new \LoggingManager(
                 DIR_FS_LOG . \grandeljayups::class . '_%1$s_%2$s.log',
                 \grandeljayups::class,
-                strtolower($LogLevel)
+                \strtolower($LogLevel)
             );
             $logger->log(
                 \get_class($throwable),
